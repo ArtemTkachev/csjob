4c41c69cc4e91efd8ed3e1717f5d5226921da1ea
TkachevArtem <art599@yandex.ru>
2020-10-17 13:01:57 +0300

    first

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+            Console.Beep();
+            string question = Console.ReadLine();
+            int size = sizeof(int);
+            Console.WriteLine(size);
+            Console.ReadKey();
+            
+        }
+    }
+}
